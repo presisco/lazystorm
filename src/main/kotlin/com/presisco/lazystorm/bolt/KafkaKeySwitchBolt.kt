5ea606b2d9333b1d064546983d0ed53e2f206fae
presisco <internight@sina.com>
2018-06-03 15:14:14 +0800

    update to 1.0.1, add KafkaKeySwitchBolt

---
+++ src/main/kotlin/com/presisco/lazystorm/bolt/KafkaKeySwitchBolt.kt
@@ -0,0 +1,30 @@
+package com.presisco.lazystorm.bolt
+
+import org.apache.storm.topology.BasicOutputCollector
+import org.apache.storm.topology.FailedException
+import org.apache.storm.topology.OutputFieldsDeclarer
+import org.apache.storm.topology.base.BaseBasicBolt
+import org.apache.storm.tuple.Fields
+import org.apache.storm.tuple.Tuple
+import org.apache.storm.tuple.Values
+import org.slf4j.LoggerFactory
+
+class KafkaKeySwitchBolt<K, V>(
+        private val kafkaKey2StreamIdMap: HashMap<K, String>
+) : BaseBasicBolt() {
+    private val logger = LoggerFactory.getLogger(KafkaKeySwitchBolt::class.java)
+
+    override fun execute(tuple: Tuple, outputCollector: BasicOutputCollector) {
+        try {
+            val streamId = kafkaKey2StreamIdMap[tuple.getValueByField("key") as K]
+            val data = tuple.getValueByField("value") as V
+            outputCollector.emit(streamId, Values(data))
+        } catch (e: Exception) {
+            throw FailedException("failed to send data", e)
+        }
+    }
+
+    override fun declareOutputFields(declarer: OutputFieldsDeclarer) {
+        kafkaKey2StreamIdMap.values.forEach { id -> declarer.declareStream(id, Fields(Constants.DATA_FIELD_NAME)) }
+    }
+}
--- src/main/kotlin/com/presisco/lazystorm/bolt/MapRenameBolt.kt
+++ src/main/kotlin/com/presisco/lazystorm/bolt/MapRenameBolt.kt
@@ -2,14 +2,14 @@ package com.presisco.lazystorm.bolt
 
 import org.apache.storm.topology.BasicOutputCollector
 import org.apache.storm.topology.FailedException
-import org.apache.storm.topology.OutputFieldsDeclarer
-import org.apache.storm.tuple.Fields
 import org.apache.storm.tuple.Tuple
 import org.apache.storm.tuple.Values
+import org.slf4j.LoggerFactory
 
 class MapRenameBolt(
         private val renameMap: HashMap<String, String>
 ) : LazyBasicBolt<Any>() {
+    private val logger = LoggerFactory.getLogger(MapRenameBolt::class.java)
 
     init {
         val intersection = renameMap.keys.intersect(renameMap.values)
