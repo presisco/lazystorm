26ae9495eb93acdc9ac38cbeba505a9044fbb109
presisco <internight@sina.com>
2019-02-19 23:12:04 +0800

    add Map Util functions to Launch

--- src/main/kotlin/com/presisco/lazystorm/Launch.kt
+++ src/main/kotlin/com/presisco/lazystorm/Launch.kt
@@ -19,6 +19,20 @@ abstract class Launch {
 
     private lateinit var stormBoot: StormBoot
 
+    private fun <T> Map<String, *>.byType(key: String): T = if (this.containsKey(key)) this[key] as T else throw IllegalStateException("$key not defined in config")
+
+    private fun Map<String, *>.getInt(key: String) = this.byType<Number>(key).toInt()
+
+    private fun Map<String, *>.getLong(key: String) = this.byType<Number>(key).toLong()
+
+    private fun Map<String, *>.getString(key: String) = this.byType<String>(key)
+
+    private fun Map<String, *>.getBoolean(key: String) = this.byType<Boolean>(key)
+
+    private fun <K, V> Map<String, *>.getHashMap(key: String) = this.byType<java.util.HashMap<K, V>>(key)
+
+    private fun <E> Map<String, *>.getList(key: String) = this.byType<List<E>>(key)
+
     /**
      * 解析命令行运行参数
      */
