cf405584f82a17b10e43c641b0a108c504b3537c
presisco <internight@sina.com>
2019-02-19 23:12:52 +0800

    change Map Util functions in Launch to protected

--- src/main/kotlin/com/presisco/lazystorm/Launch.kt
+++ src/main/kotlin/com/presisco/lazystorm/Launch.kt
@@ -19,19 +19,19 @@ abstract class Launch {
 
     private lateinit var stormBoot: StormBoot
 
-    private fun <T> Map<String, *>.byType(key: String): T = if (this.containsKey(key)) this[key] as T else throw IllegalStateException("$key not defined in config")
+    protected fun <T> Map<String, *>.byType(key: String): T = if (this.containsKey(key)) this[key] as T else throw IllegalStateException("$key not defined in config")
 
-    private fun Map<String, *>.getInt(key: String) = this.byType<Number>(key).toInt()
+    protected fun Map<String, *>.getInt(key: String) = this.byType<Number>(key).toInt()
 
-    private fun Map<String, *>.getLong(key: String) = this.byType<Number>(key).toLong()
+    protected fun Map<String, *>.getLong(key: String) = this.byType<Number>(key).toLong()
 
-    private fun Map<String, *>.getString(key: String) = this.byType<String>(key)
+    protected fun Map<String, *>.getString(key: String) = this.byType<String>(key)
 
-    private fun Map<String, *>.getBoolean(key: String) = this.byType<Boolean>(key)
+    protected fun Map<String, *>.getBoolean(key: String) = this.byType<Boolean>(key)
 
-    private fun <K, V> Map<String, *>.getHashMap(key: String) = this.byType<java.util.HashMap<K, V>>(key)
+    protected fun <K, V> Map<String, *>.getHashMap(key: String) = this.byType<java.util.HashMap<K, V>>(key)
 
-    private fun <E> Map<String, *>.getList(key: String) = this.byType<List<E>>(key)
+    protected fun <E> Map<String, *>.getList(key: String) = this.byType<List<E>>(key)
 
     /**
      * 解析命令行运行参数
