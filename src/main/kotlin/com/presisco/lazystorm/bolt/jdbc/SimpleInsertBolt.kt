206bd648b8c523cfb53565a7d20f96a48d14093a
presisco <internight@sina.com>
2019-03-15 15:45:59 +0800

    update lazyjdbc 1.1.0, default use yyyy-MM-dd HH:mm:ss.SSS add count mode in LazyWindowedBolt

--- src/main/kotlin/com/presisco/lazystorm/bolt/jdbc/SimpleInsertBolt.kt
+++ src/main/kotlin/com/presisco/lazystorm/bolt/jdbc/SimpleInsertBolt.kt
@@ -1,13 +1,14 @@
 package com.presisco.lazystorm.bolt.jdbc
 
 import com.presisco.lazyjdbc.client.MapJdbcClient
+import com.presisco.lazystorm.bolt.Constants
 import org.apache.storm.topology.BasicOutputCollector
 import org.slf4j.LoggerFactory
 
-class SimpleInsertBolt : JdbcClientBolt<MapJdbcClient>() {
+open class SimpleInsertBolt : JdbcClientBolt<MapJdbcClient>() {
     private val logger = LoggerFactory.getLogger(SimpleInsertBolt::class.java)
 
-    override fun loadJdbcClient() = MapJdbcClient(dataSource, queryTimeout, rollbackOnBatchFailure)
+    override fun loadJdbcClient() = MapJdbcClient(dataSource, queryTimeout, rollbackOnBatchFailure).withDateFormat(Constants.timeStampFormat)
 
     override fun process(data: List<*>, table: String, client: MapJdbcClient, collector: BasicOutputCollector): List<*> {
         val failedSet = client.insert(table, data as List<Map<String, Any?>>)
--- src/main/kotlin/com/presisco/lazystorm/bolt/jdbc/SimpleReplaceBolt.kt
+++ src/main/kotlin/com/presisco/lazystorm/bolt/jdbc/SimpleReplaceBolt.kt
@@ -1,13 +1,14 @@
 package com.presisco.lazystorm.bolt.jdbc
 
 import com.presisco.lazyjdbc.client.MapJdbcClient
+import com.presisco.lazystorm.bolt.Constants
 import org.apache.storm.topology.BasicOutputCollector
 import org.slf4j.LoggerFactory
 
-class SimpleReplaceBolt : JdbcClientBolt<MapJdbcClient>() {
+open class SimpleReplaceBolt : JdbcClientBolt<MapJdbcClient>() {
     private val logger = LoggerFactory.getLogger(SimpleReplaceBolt::class.java)
 
-    override fun loadJdbcClient() = MapJdbcClient(dataSource, queryTimeout, rollbackOnBatchFailure)
+    override fun loadJdbcClient() = MapJdbcClient(dataSource, queryTimeout, rollbackOnBatchFailure).withDateFormat(Constants.timeStampFormat)
 
     override fun process(data: List<*>, table: String, client: MapJdbcClient, collector: BasicOutputCollector): List<*> {
         val failedSet = client.replace(table, data as List<Map<String, Any?>>)
