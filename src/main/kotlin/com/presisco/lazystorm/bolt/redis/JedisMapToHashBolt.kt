da332fe6e1cc9113723661c06c10f2c55443de5e
presisco <internight@sina.com>
2019-04-02 16:20:29 +0800

    fix wrong tuple data handling in JedisMapToHashBolt

--- src/main/kotlin/com/presisco/lazystorm/bolt/redis/JedisMapToHashBolt.kt
+++ src/main/kotlin/com/presisco/lazystorm/bolt/redis/JedisMapToHashBolt.kt
@@ -1,17 +1,30 @@
 package com.presisco.lazystorm.bolt.redis
 
+import com.presisco.gsonhelper.FuzzyHelper
+import com.presisco.lazystorm.mapValueToHashMap
+import org.apache.storm.task.TopologyContext
 import org.apache.storm.topology.BasicOutputCollector
 import org.apache.storm.tuple.Tuple
 
-class JedisMapToHashBolt : JedisSingletonBolt<HashMap<String, Any?>>() {
+class JedisMapToHashBolt : JedisSingletonBolt<Map<String, *>>() {
+    @Transient
+    lateinit var fuzzyHelper: FuzzyHelper
+
+    override fun prepare(stormConf: MutableMap<*, *>, context: TopologyContext) {
+        super.prepare(stormConf, context)
+        fuzzyHelper = FuzzyHelper()
+    }
 
     override fun execute(tuple: Tuple, outputCollector: BasicOutputCollector) {
-        val hashMap = hashMapOf<String, String>()
-        tuple.fields.forEachIndexed { index, field ->
-            hashMap[field] = tuple.getValue(index).toString()
-        }
         val jedisCmd = getCommand()
-        jedisCmd.hmset(getKey(tuple.sourceStreamId), hashMap)
+        jedisCmd.hmset(getKey(tuple.sourceStreamId), getInput(tuple).mapValueToHashMap { value ->
+            when (value) {
+                is String -> value
+                is Map<*, *> -> fuzzyHelper.toJson(value)
+                is List<*> -> fuzzyHelper.toJson(value)
+                else -> value.toString()
+            }
+        })
         closeCommand(jedisCmd)
     }
 
