1af7e4b78f5b16dcdeaf63285752fa906f8189c6
presisco <internight@sina.com>
2019-11-16 11:48:04 +0800

    update to 1.6.2, add optional connector create callback in Launch

--- src/main/kotlin/com/presisco/lazystorm/Launch.kt
+++ src/main/kotlin/com/presisco/lazystorm/Launch.kt
@@ -1,6 +1,7 @@
 package com.presisco.lazystorm
 
 import com.presisco.gsonhelper.ConfigMapHelper
+import com.presisco.lazystorm.topology.LazyTopoBuilder
 import org.apache.storm.topology.IComponent
 import org.apache.storm.topology.IRichSpout
 
